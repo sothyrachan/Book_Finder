b540f6601226b8f0603b3d18f9fdd53f3f5a9d62
Sothyra12 <schan256@my.centennialcollege.ca>
2025-05-06 21:53:21 -0400

    created new variables based on their id

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import "./output.css";
+
+const searchForm = document.getElementById("search-form") as HTMLFormElement;
+const searchInput = document.getElementById("search-input") as HTMLInputElement;
+const searchBtn = document.getElementById("search-btn") as HTMLButtonElement;
+const bookContainer = document.getElementById("book-result") as HTMLDivElement;
